d562477e6b6e929c2438e89f8d29ae561defbe2b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-08-29 01:50:32 +0900

    fix

--- nuget/BuildUtils.ps1
+++ nuget/BuildUtils.ps1
@@ -146,8 +146,8 @@ class Config
 
    static $BuildLibraryIOSHash =
    @{
-      # "DlibDotNet.Native"     = "libDlibDotNetNative.a";
-      # "DlibDotNet.Native.Dnn" = "libDlibDotNetNativeDnn.a";
+      "DlibDotNet.Native"     = "libDlibDotNetNative.a";
+      "DlibDotNet.Native.Dnn" = "libDlibDotNetNativeDnn.a";
       "DlibDotNet.Native.Lite" = "libDlibDotNetNativeLite.a";
    }
 
