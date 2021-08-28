99d36ef43152ed5ca2f3b18c946fc7b513c3c0ed
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-08-29 06:14:16 +0900

    fix

--- src/DlibDotNet/Dnn/LossMulticlassLog.cs
+++ src/DlibDotNet/Dnn/LossMulticlassLog.cs
@@ -1,4 +1,3 @@
-#if !LITE
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -332,6 +331,7 @@ namespace DlibDotNet.Dnn
             throw new NotSupportedException();
         }
 
+#if !LITE
         public static void TestOneStep<T>(DnnTrainer<LossMulticlassLog> trainer, IEnumerable<Matrix<T>> data, IEnumerable<uint> label)
             where T : struct
         {
@@ -454,6 +454,7 @@ namespace DlibDotNet.Dnn
                 }
             }
         }
+#endif
 
         #region Overrides 
 
@@ -755,4 +756,3 @@ namespace DlibDotNet.Dnn
     }
 
 }
-#endif
--- src/DlibDotNet/PInvoke/Dnn/Loss/LossMulticlassLog.cs
+++ src/DlibDotNet/PInvoke/Dnn/Loss/LossMulticlassLog.cs
@@ -1,4 +1,3 @@
-#if !LITE
 using System;
 using System.Runtime.InteropServices;
 using OutputLabelType = System.UInt32;
@@ -252,4 +251,3 @@ namespace DlibDotNet
     }
 
 }
-#endif
